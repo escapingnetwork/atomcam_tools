2b90bf88ec4efd004359c6c95de536ea555cf1cf
Leo <leandro.martin@protonmail.com>
2023-06-04 12:43:46 -0300

    include hack.ini on release

--- buildscripts/post_image.sh
+++ buildscripts/post_image.sh
@@ -2,10 +2,11 @@
 set -e
 
 cd output/images
-echo "atomcam" > hostname
+echo "personalcam" > hostname
 touch authorized_keys
 cp -dpf uImage.lzma factory_t31_ZMC6tiIDQN
+cp /src/hack.ini hack.ini
 mv rootfs.ext2 rootfs_hack.ext2
 rm -f /src/atomcam_tools.zip
-zip -ry /src/atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
+zip -ry /src/atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys hack.ini
 cp -f factory_t31_ZMC6tiIDQN rootfs_hack.ext2 /src/target
